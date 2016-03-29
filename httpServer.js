155a0ca273b4f2a03f3379eceef35222c890ce23
txx3258 <soft_tangxiaoxian@163.com>
2016-03-29 09:29:35 +0800

    mult

--- httpServer.js
+++ httpServer.js
@@ -1,21 +1,89 @@
 'use strict';
 
 let http=require('http');
-let url=require('url');
+let URL=require('url');
 let redis=require('redis');
 let log4js=require('log4js');
 
 let addon = require('./build/Release/addon.node');
+let CONF=require('./config');
+
+//配置log4js
+log4js.configure({
+  appenders:[
+    {
+      "type":"file",
+      "filename":"./logs/server.log"
+      "maxLogSize":CONF.maxLogSize,
+      "backups":2,
+      "category":"sys"
+    }
+  ]
+});
+
+let logSys=log4js.getLogger('sys'); 
+
+/**
+ *redis客户端
+ */
+let client=undefined;
+function conn(){
+  client=redis.createClient(CONF.REDIS_PORT,CONF.REDIS_IP,CONF.REDIS_CONF);
+
+  client.on('error',function(err){
+    client=undefined;
+    logSys.error('redis conn error.');
+    setTimeout(conn,5000);
+  });
+}
 
 /**
  *建立http服务器
  */
 let server=http.createServer(function(req,res){
-  let cid=url.parse(req.url).query;
+  let url=req.url;
+  //访问记录
+  logSys.info(url);
 
+  let cid=URL.parse(req.url).query;
   let keys=addon.hashkeys(cid,cid.length);  
+  
+  let Keys=keys.map(function(item){
+    return ['setbit',"boomfilter",item,1];
+  });
 
+  if (!client){
+    res.writeHead(500,{}).end('redis is null');
+    return;
+  }
+  
+  Keys.forEach(function(item){
+     multi.setbit('bloomfilter',item,1); 
+  });
+
+  multi=client.multi();
+  multi.exec(function(err,reply){
+    if (!err){
+      print("{'success':false,'cid':"+cid+"}");
+    }else{
+      print("{'success':true,'cid':''}")
+    }
+
+    //打印结果
+    function print(result){
+      res.writeHead(200,{'Content-Type':'application/json'});
+      res.end(result);
+    }
+  });
 });
 
 //监听端口
-server.listen(PORT);
+server.listen(CONF.HTTP_PORT);
+
+/**
+ *异常处理
+ */
+process.on('uncaughtException',function(err){
+  logSys.error('uncaughtException:'+err);
+});
+
